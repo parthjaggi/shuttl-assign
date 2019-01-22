26ed68b49842b799757ab0eb49648861d7438c7c
Parth <parthjaggi28@gmail.com>
2019-01-22 21:44:49 +0530

    assume_role;

--- provider_config.tf
+++ provider_config.tf
@@ -11,4 +11,8 @@ provider "aws" {
   access_key = "${var.provider["access_key"]}"
   secret_key = "${var.provider["secret_key"]}"
   region     = "${var.provider["region"]}"
+
+  # assume_role {
+  #   role_arn = "arn:aws:iam::1234567890:role/TerraformRuntimeRole"
+  # }
 }
